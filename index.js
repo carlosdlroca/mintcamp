86d61d1b8b6dde279fe42e1e281809779697a7aa
Carlos De La Roca <carlosdlroca80@gmail.com>
2022-01-01 13:39:02 -0800

    add default styles

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ const app = express()
 const path = require("path");
 
 app.set("view engine", "ejs");
-app.use(path.join(__dirname, "public"))
+app.use(express.static(path.join(__dirname, "public")))
+
+
+app.get("/", (req, res) => {
+    res.render("landing");
+})
+
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
